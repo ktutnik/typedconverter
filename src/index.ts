7d39c34e4a27d7dab058f7525808b1705ec9391b
Ketut Sandiarsa <ktutnik@gmail.com>
2019-04-01 19:23:35 +0800

    Simplified converter parameters

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,13 @@ interface ConverterMap { key: Function, converter: Converter }
 interface ConversionIssue { path: string[], messages: string[] }
 type ConversionResult = [any, ConversionIssue[] | undefined]
 type ConverterStore = Map<Function | string, Converter>
+interface ObjectInfo<T> {
+    path: string[],
+    expectedType: T,
+    converters: ConverterStore
+}
 
-type Converter = (value: any, path: string[], expectedType: Function | Function[], converters: ConverterStore) => ConversionResult
+type Converter = (value: any, info: ObjectInfo<Function | Function[]>) => ConversionResult
 
 class ConversionError extends Error {
     constructor(public issues: ConversionIssue[], public status = 400) {
@@ -56,49 +61,54 @@ function createInstance<T extends Class>(value: any, TheType: T) {
 // --------------------------------------------------------------------- //
 
 namespace DefaultConverters {
-    export function booleanConverter(rawValue: {}, path: string[]): ConversionResult {
+    export function booleanConverter(rawValue: {}, info: ObjectInfo<Function | Function[]>): ConversionResult {
         const value: string = safeToString(rawValue)
         const list: { [key: string]: boolean | undefined } = {
             on: true, true: true, "1": true, yes: true,
             off: false, false: false, "0": false, no: false
         }
         const result = list[value.toLowerCase()]
-        if (result === undefined) return [undefined, createConversionError(value, Boolean, path)]
+        if (result === undefined) return [undefined, createConversionError(value, Boolean, info.path)]
         return [result, undefined]
     }
 
-    export function numberConverter(rawValue: {}, path: string[]): ConversionResult {
+    export function numberConverter(rawValue: {}, info: ObjectInfo<Function | Function[]>): ConversionResult {
         const value = safeToString(rawValue)
         const result = Number(value)
-        if (isNaN(result) || value === "") return [undefined, createConversionError(value, Number, path)]
+        if (isNaN(result) || value === "") return [undefined, createConversionError(value, Number, info.path)]
         return [result, undefined]
     }
 
-    export function dateConverter(rawValue: {}, path: string[]): ConversionResult {
+    export function dateConverter(rawValue: {}, info: ObjectInfo<Function | Function[]>): ConversionResult {
         const value = safeToString(rawValue)
         const result = new Date(value)
-        if (isNaN(result.getTime()) || value === "") return [undefined, createConversionError(value, Date, path)]
+        if (isNaN(result.getTime()) || value === "") return [undefined, createConversionError(value, Date, info.path)]
         return [result, undefined]
     }
 
-    export function modelConverter(value: {}, path: string[], expectedType: Function | Function[], converters: ConverterStore): ConversionResult {
+    export function modelConverter(value: {}, info: ObjectInfo<Function | Function[]>): ConversionResult {
+        const { converters } = info
         //--- helper functions
         const isConvertibleToObject = (value: any) =>
             typeof value !== "boolean"
             && typeof value !== "number"
             && typeof value !== "string"
         //---
-        const TheType = expectedType as Class
+        const TheType = info.expectedType as Class
         //get reflection metadata of the class
         const reflection = reflect(TheType)
         //check if the value is possible to convert to model
-        if (!isConvertibleToObject(value)) return [undefined, createConversionError(value, TheType, path)]
+        if (!isConvertibleToObject(value)) return [undefined, createConversionError(value, TheType, info.path)]
         const instance = createInstance(value, TheType)
         //traverse through the object properties and convert to appropriate property's type
         //sanitize excess property to prevent object having properties that doesn't match with declaration
         const issues: ConversionIssue[] = []
         for (let x of reflection.properties) {
-            const [val, err] = convert((value as any)[x.name], path.concat(x.name), x.type, converters)
+            const [val, err] = convert((value as any)[x.name], {
+                path: info.path.concat(x.name),
+                expectedType: x.type,
+                converters
+            })
             if (err) issues.push(...err)
             //remove undefined properties
             if (val === undefined) delete instance[x.name]
@@ -108,8 +118,12 @@ namespace DefaultConverters {
         else return [instance, undefined]
     }
 
-    function arrayConverter(value: {}[], path: string[], expectedType: Function[], converters: ConverterStore): ConversionResult {
-        const result = value.map((x, i) => convert(x, path.concat(i.toString()), expectedType[0], converters))
+    function arrayConverter(value: {}[], info: ObjectInfo<Function[]>): ConversionResult {
+        const result = value.map((x, i) => convert(x, {
+            path: info.path.concat(i.toString()),
+            expectedType: info.expectedType && info.expectedType[0],
+            converters: info.converters
+        }))
         if (result.some(x => !!x[1])) {
             const issue: ConversionIssue[] = []
             for (const [, err] of result) {
@@ -121,14 +135,14 @@ namespace DefaultConverters {
             return [result.map(x => x[0]), undefined]
     }
 
-    export function strictArrayConverter(value: {}, path: string[], expectedType: Function[], converters: ConverterStore): ConversionResult {
-        if (!Array.isArray(value)) return [undefined, createConversionError(value, expectedType, path)]
-        return arrayConverter(value, path, expectedType, converters)
+    export function strictArrayConverter(value: {}, info: ObjectInfo<Function[]>): ConversionResult {
+        if (!Array.isArray(value)) return [undefined, createConversionError(value, info.expectedType, info.path)]
+        return arrayConverter(value, info)
     }
 
-    export function friendlyArrayConverter(value: {}, path: string[], expectedType: Function[], converters: ConverterStore): ConversionResult {
+    export function friendlyArrayConverter(value: {}, info: ObjectInfo<Function[]>): ConversionResult {
         const cleanValue = Array.isArray(value) ? value : [value]
-        return arrayConverter(cleanValue, path, expectedType, converters)
+        return arrayConverter(cleanValue, info)
     }
 }
 
@@ -140,20 +154,21 @@ function validate(validator: (val: any, type: Function) => Promise<undefined | s
 // --------------------------- MAIN CONVERTER -------------------------- //
 // --------------------------------------------------------------------- //
 
-function convert(value: any, path: string[], expectedType: Function | Function[] | undefined, converters: ConverterStore): ConversionResult {
+function convert(value: any, info: ObjectInfo<Function | Function[] | undefined>): ConversionResult {
+    const { expectedType, converters, path } = info
     if (value === null || value === undefined) return [undefined, undefined]
     if (!expectedType) return [value, undefined]
     if (expectedType === Object) return [value, undefined]
     if (value.constructor === expectedType) return [value, undefined]
     //check if the parameter contains @array()
     if (Array.isArray(expectedType))
-        return converters.get("Array")!(value, path, expectedType, converters)
+        return converters.get("Array")!(value, { expectedType, converters, path })
     //check if parameter is native value that has default converter (Number, Date, Boolean) or if user provided converter
     else if (converters.has(expectedType))
-        return converters.get(expectedType)!(value, path, expectedType, converters)
+        return converters.get(expectedType)!(value, { expectedType, converters, path })
     //if type of model and has no  converter, use DefaultObject converter
     else
-        return converters.get("Model")!(value, path, expectedType as Class, converters)
+        return converters.get("Model")!(value, { expectedType, converters, path })
 }
 
 function converter(option: { guessArrayElement?: boolean, type?: Function | Function[], converters?: ConverterMap[] } = {}) {
@@ -166,7 +181,7 @@ function converter(option: { guessArrayElement?: boolean, type?: Function | Func
         mergedConverters.set("Array", arrayConverter);
         mergedConverters.set("Model", DefaultConverters.modelConverter);
         (option.converters || []).forEach(x => mergedConverters.set(x.key, x.converter))
-        const [val, err] = convert(value, path, type || option.type, mergedConverters)
+        const [val, err] = convert(value, { path, expectedType: type || option.type, converters: mergedConverters })
         if (err) throw new ConversionError(err)
         else return val
     }
