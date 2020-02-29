ffb9ad4f59ec1be38706ffd4e7a6969736e74d2c
Ketut Sandiarsa <ktutnik@gmail.com>
2020-02-29 18:47:06 +0800

    chore: Upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  //testEnvironment: 'node',
   collectCoverage:true,
   collectCoverageFrom: ["src/*"],
   snapshotSerializers: [
